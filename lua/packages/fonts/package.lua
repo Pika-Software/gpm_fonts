e66225705359b47b39a2e0902b2f8d78c40fed68
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-05-09 01:28:01 +0400

    1.1.1

--- lua/packages/fonts/package.lua
+++ lua/packages/fonts/package.lua
@@ -2,5 +2,9 @@ Name = "Fonts"
 Description = "More convenient font creation."
 
 Author = "PrikolMen:-b"
-Version = "1.1.0"
+Version = "1.1.1"
 License = "MIT"
+
+Dependencies = {
+    ["Game Ready"] = ">=1.4.0"
+}
